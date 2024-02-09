c78f7fc4aba15fe2db0e8e04f91becfa8aac7174
Yaroslav <sych521@gmail.com>
2024-02-09 16:43:20 +0200

    A fix for header

--- js/stickyNavigationHandler.js
+++ js/stickyNavigationHandler.js
@@ -4,14 +4,12 @@ window.addEventListener("scroll", () => {
 	let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 	let screenWidth = window.innerWidth || document.documentElement.clientWidth;
 
-	if (currentScroll && screenWidth) {
-		if (screenWidth > 767 || screenWidth <= 767) {
-			if (currentScroll > lastScrollTop) {
-				document.getElementsByTagName('header')[0].style.transform = "translateY(-105%)";
-			} else {
-				document.getElementsByTagName('header')[0].style.transform = "translateY(0)";
-			}
-			lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Safari
+	if (currentScroll > lastScrollTop) {
+		if (currentScroll > 0 && screenWidth > 767) {
+			document.getElementsByTagName('header')[0].style.transform = "translateY(-105%)";
 		}
+	} else {
+		document.getElementsByTagName('header')[0].style.transform = "translateY(0)";
 	}
+	lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Safari
 }, false);
