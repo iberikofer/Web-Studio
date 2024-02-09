1569b7dff3e228105e220fbcf6f3501e2faf687a
Yaroslav <sych521@gmail.com>
2024-02-09 13:22:26 +0200

    some fixes for code to look better

--- js/stickyNavigationHandler.js
+++ js/stickyNavigationHandler.js
@@ -4,12 +4,14 @@ window.addEventListener("scroll", () => {
 	let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 	let screenWidth = window.innerWidth || document.documentElement.clientWidth;
 
-	if (screenWidth > 1200) {
-		if (currentScroll > lastScrollTop) {
-			document.getElementsByTagName('header')[0].style.transform = "translateY(-105%)";
-		} else {
-			document.getElementsByTagName('header')[0].style.transform = "translateY(0)";
+	if (currentScroll && screenWidth) {
+		if (screenWidth > 767 || screenWidth <= 767) {
+			if (currentScroll > lastScrollTop) {
+				document.getElementsByTagName('header')[0].style.transform = "translateY(-105%)";
+			} else {
+				document.getElementsByTagName('header')[0].style.transform = "translateY(0)";
+			}
+			lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Safari
 		}
-		lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Safari
 	}
 }, false);
